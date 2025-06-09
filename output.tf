00c25ddb4edac07a9dab34efa8c3e1fcbbadf553
AdaEde22 <Jnoyima@gmail.com>
2025-06-09 08:34:24 +0100

    Add files via upload

--- output.tf
+++ output.tf
@@ -1,4 +1,10 @@
-output "route53_name_servers" {
-    description = "The name servers of the Route 53 hosted zone"
-    value       = aws_route53_zone.r53_zone.name_servers
+output "nginx_ingress_lb_dns" {
+  value = data.aws_lb.nginx_ingress.dns_name
+}
+
+output "nginx_lb_ip" {
+  value = data.aws_lb.nginx_ingress.ip_address_type == "ipv4" ? data.aws_lb.nginx_ingress.dns_name : ""
+}
+output "nginx_ingress_load_balancer_hostname" {
+  value = data.aws_lb.nginx_ingress.dns_name
 }
